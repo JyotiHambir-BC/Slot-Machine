7f13b5f42e966a878cbfe0a585794e829b684625
Jyoti Hambir <qdde3t1v@students.codeinstitute.net>
2024-09-06 09:02:10 +0000

    add max_line function

--- sm.py
+++ sm.py
@@ -1,3 +1,7 @@
+MAX_LINE = 3
+MAX_BET = 100
+MIN_BET = 1
+
 def deposite():
     while True:
         amount = input("What would you like to deposit? £")
@@ -11,3 +15,24 @@ def deposite():
             print("Please enter a number")
 
     return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINE) +")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINE:
+                break
+            else:
+                print("Please enter a valid line number")
+        else:
+            print("Please enter a number")
+
+    return lines
+
+def main():
+    balance = deposite()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+main()
